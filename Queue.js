94cd33212a895c07f9d41c87cc0e9f4befdfa1c3
Zachary Mathis <zachamathis@gmail.com>
2022-11-26 18:16:09 -0800

    added the queue data structure

---
+++ Queue.js
@@ -0,0 +1,27 @@
+import LinkedList from "./LinkedList";
+
+export default class Queue {
+  constructor(maxSize = Infinity) {
+    this.queue = new LinkedList();
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  enqueue(data) {
+    if (this.hasRoom()) {
+      this.queue.addToTail(data);
+      this.size++;
+      console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+    } else {
+      throw new Error('Queue is full!')
+    }
+  }
+}
