b665c27781c8b74c6c421ede894642578f0f18d6
Zachary Mathis <zachamathis@gmail.com>
2022-11-26 17:34:44 -0800

    beautified existing code

--- LinkedList.js
+++ LinkedList.js
@@ -7,15 +7,17 @@ export default class LinkedList {
 
   addToHead(data) {
     const newHead = new Node(data);
+    
     const currentHead = this.head;
+
     this.head = newHead;
-    if (currentHead) {
-      this.head.setNextNode(currentHead);
-    }
+    
+    if (currentHead) this.head.setNextNode(currentHead);
   }
 
   addToTail(data) {
     let tail = this.head;
+
     if (!tail) {
       this.head = new Node(data);
     } else {
@@ -24,25 +26,30 @@ export default class LinkedList {
       }
       tail.setNextNode(new Node(data));
     }
+
   }
 
   removeHead() {
     const removedHead = this.head;
-    if (!removedHead) {
-      return;
-    }
+    
+    if (!removedHead) return;
+
     this.head = removedHead.getNextNode();
+
     return removedHead.data;
   }
 
   printList() {
     let currentNode = this.head;
     let output = '<head> ';
+
     while (currentNode !== null) {
       output += currentNode.data + ' ';
       currentNode = currentNode.getNextNode();
     }
+
     output += '<tail>';
+
     console.log(output);
   }
 
