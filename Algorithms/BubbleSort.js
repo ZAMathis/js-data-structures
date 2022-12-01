598b206cb103c6b9b172975f9ecd03a26e82f1e6
Zachary Mathis <zachamathis@gmail.com>
2022-12-01 15:21:35 -0800

    made algorithms folder, and bubblesort.js

---
+++ Algorithms/BubbleSort.js
@@ -0,0 +1,26 @@
+// gonna have to write this code better. i'm not a fan.
+
+const swap = (arr, indexOne, indexTwo) => {
+    const temp = arr[indexTwo];
+    arr[indexTwo] = arr[indexOne];
+    arr[indexOne] = temp;
+};
+
+export const bubbleSort = input => {
+    let swapCount = 0
+    let swapping = true;
+    
+    while (swapping) {
+        swapping = false;
+        for (let i = 0; i < input.length - 1; i++) {
+            if (input[i] > input[i + 1]) {
+                swap(input, i, i + 1);
+                swapCount++;
+                swapping = true;
+            }
+        }
+    }
+    console.log(`Swapped ${swapCount} times`);
+    return input;
+};
+
